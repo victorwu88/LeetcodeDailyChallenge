f677463585f487146e17b48c24714369c29405f2
victorwu88 <40125702+victorwu88@users.noreply.github.com>
2021-02-18 00:46:44 -0600

    Add files via upload

---
+++ LC526.py
@@ -0,0 +1,22 @@
+class Solution:
+    count = 0
+
+    def countArrangement(self, n: int) -> int:
+
+        visited = [False] * (n + 1)
+        self.calculate(n, 1, visited)
+        return self.count
+
+    def calculate(self, n, pos, visited):
+        if pos > n:
+            self.count += 1
+
+        for i in range(1, n + 1):
+            if not visited[i] and (pos % i == 0 or i % pos == 0):
+                visited[i] = True
+                self.calculate(n, pos + 1, visited)
+                visited[i] = False
+
+n = 5
+a = Solution()
+print(a.countArrangement(n))
